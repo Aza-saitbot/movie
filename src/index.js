9bb8a36a71aed9059f4c86995c9e7b9c04dd0d18
Saitbot <73251805+Aza-saitbot@users.noreply.github.com>
2021-07-20 09:34:56 +0300

    for static hosting HashRouter

--- src/index.js
+++ src/index.js
@@ -2,13 +2,13 @@ import 'bootstrap/dist/css/bootstrap.min.css';
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
-import {BrowserRouter} from 'react-router-dom'
+import {HashRouter} from 'react-router-dom'
 
 
 
 ReactDOM.render(
-  <BrowserRouter>
+  <HashRouter>
     <App />
-  </BrowserRouter>,
+  </HashRouter>,
   document.getElementById('root')
 );
